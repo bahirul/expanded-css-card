1aa64500afc9739ac1be6946b528133eee9a73e1
Bahirul Rizki Ariyanto <ari@detelnetworks.id>
2022-01-30 23:05:06 +0800

    update README

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,19 @@
+// default selector card
 const expandingCard = document.querySelectorAll('.expanding-card');
+
+// selector for active card
 const expandingCardActive = document.querySelectorAll('.expanding-card');
 
-expandingCard.forEach(card => {
-    card.addEventListener('click', () => {
-        // remove prev active class
-        expandingCardActive.forEach(removeActive => {
-            removeActive.classList.remove('active');
-        })
+window.onload = () => {
+    expandingCard.forEach(card => {
+        card.addEventListener('click', () => {
+            // remove prev active class
+            expandingCardActive.forEach(removeActive => {
+                removeActive.classList.remove('active');
+            })
 
-        // add active class
-        card.classList.add('active');
-    })
-});
+            // add active class
+            card.classList.add('active');
+        })
+    });
+}
